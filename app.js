0d620ee9597b69fcf65592dba500d3708dce722b
Jacob <jacobmen416@yahoo.com>
2020-07-03 14:57:32 -0500

    redo jumbo, add particles, new colors for website

---
+++ app.js
@@ -0,0 +1,7 @@
+particlesJS.load('particles-js', 'assets/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+
+$(document).ready(function () {
+    $('body').bootstrapMaterialDesign();
+});
