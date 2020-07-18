511b59dde4ac61936060d6c4a2fa38718bea9534
Jacob <jacobmen416@yahoo.com>
2020-07-18 13:45:49 -0500

    add extracurriculars

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ particlesJS.load('particles-js', 'assets/particles.json', function () {
 $(document).ready(function () {
     $('body').bootstrapMaterialDesign();
 });
+
+$(document).ready(function(){
+    $(".extracurr-btn").click(function(){
+        if ($(this).hasClass('active')){
+            $('#' + this.hash.substr(1).toLowerCase()).toggleClass('active');
+        }
+    });
+});
