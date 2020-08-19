a3dfcd98561d824963d80073917025cc980716ff
Jacob <jacobmen416@yahoo.com>
2020-08-19 13:00:49 -0500

    add event

--- app.js
+++ app.js
@@ -5,11 +5,3 @@ particlesJS.load('particles-js', 'assets/particles.json', function () {
 $(document).ready(function () {
     $('body').bootstrapMaterialDesign();
 });
-
-$(document).ready(function(){
-    $(".extracurr-btn").click(function(){
-        if ($(this).hasClass('active')){
-            $('#' + this.hash.substr(1).toLowerCase()).toggleClass('active');
-        }
-    });
-});
